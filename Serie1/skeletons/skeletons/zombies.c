a5794107276510ba35519151814caf89417697d0
SteveMuersetUnibe <steve.muerset@students.unibe.ch>
2016-03-12 17:29:15 +0100

    added serie 1

---
+++ Serie1/skeletons/skeletons/zombies.c
@@ -0,0 +1,18 @@
+/* gcc -Wall -std=gnu99 zombies.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void create_zombie() {
+	/* TODO: Implement child process and kill it */
+	return;
+}
+
+int main(void)
+{
+	int wait = 15;
+	/* TODO: implement */
+
+	return 0;
+}
